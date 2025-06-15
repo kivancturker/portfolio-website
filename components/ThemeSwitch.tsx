888a1985ee0721370962ac6b7d99d5674639ebaa
Kıvanç TÜRKER <kivanc.turker1997@gmail.com>
2025-06-16 01:16:59 +0300

    feat: Implemented Ligh/Dark Theme support

---
+++ components/ThemeSwitch.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { Button, IconButton } from "@radix-ui/themes";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { Sun, Moon } from "lucide-react";
+
+export const ThemeSwitch = () => {
+  const [mounted, setMounted] = useState<boolean>(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  if (!mounted) {
+    return null;
+  }
+
+  return <ThemeButton theme={theme} setTheme={setTheme} />;
+};
+
+function ThemeButton({
+  theme,
+  setTheme,
+}: {
+  theme: string | undefined;
+  setTheme: (theme: string) => void;
+}) {
+  return (
+    <IconButton
+      variant="ghost"
+      onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+    >
+      {theme === "light" ? <Sun /> : <Moon />}
+    </IconButton>
+  );
+}
